0fac0446c9c0b2b7c02ebf8e74b6322cbdfd3522
Varshini Sivapu <varshinisivapu0@gmail.com>
2025-06-23 18:39:26 +0530

    Deploy production build with correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         entryFileNames: 'assets/[name].js',
+//         chunkFileNames: 'assets/[name].js',
+//         assetFileNames: 'assets/[name].[ext]'
+//       }
+//     }
+//   }
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
+  base: '/my-website/', 
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
